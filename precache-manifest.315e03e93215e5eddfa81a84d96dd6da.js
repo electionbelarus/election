e40bed906aa3f4689c8c59b1b9be28ac258ccb1d
electionbelarus <electionbelarus@gmail.com>
2020-07-21 10:16:14 +0300

    #2

---
+++ precache-manifest.315e03e93215e5eddfa81a84d96dd6da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843ddd2835b5d55ff2d396d686d6ce8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a8ff0733e15c593600",
+    "url": "/static/css/main.22ed3b17.chunk.css"
+  },
+  {
+    "revision": "709d112e0395f5597c8a",
+    "url": "/static/js/2.07c876c6.chunk.js"
+  },
+  {
+    "revision": "b2490fbb34248c3403116f401942d851",
+    "url": "/static/js/2.07c876c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a8ff0733e15c593600",
+    "url": "/static/js/main.6ec0b7ad.chunk.js"
+  },
+  {
+    "revision": "8df2b922e53787820e70",
+    "url": "/static/js/runtime-main.cc592715.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "366c6bf5456eaba371d2c728deb37684",
+    "url": "/static/media/logo.366c6bf5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/election/precache-manifest.0b27740fb09d9f4383a6b27efeaa72fd.js"
+  "/precache-manifest.315e03e93215e5eddfa81a84d96dd6da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/election/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
